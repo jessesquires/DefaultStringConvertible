16885b7e0973b5e603f20b94386200ed25227968
Jesse Squires <jesse.d.squires@gmail.com>
2016-04-25 19:52:23 -0700

    tests. schemes

--- Tests/DefaultStringConvertibleTests.swift
+++ Tests/DefaultStringConvertibleTests.swift
@@ -18,12 +18,29 @@
 
 import XCTest
 
-@testable import DefaultStringConvertible
+import DefaultStringConvertible
 
 
 final class DefaultStringConvertibleTests: XCTestCase {
 
-    func testExample() {
+    func test_thatClassProvidesDefaultDescription() {
+        let c = MyClass()
+        let description = c.description
 
+        XCTAssertEqual(description, "MyClass(myString: \"my string var\", myInt: 666, myDouble: 42.0, myChar: c)")
+
+        print("\n", c, "\n")
     }
+}
+
+
+
+// MARK: Fakes
+
+class MyClass: CustomStringConvertible {
+
+    let myString = "my string var"
+    let myInt = 666
+    let myDouble = 42.0
+    let myChar = Character("c")
 }
