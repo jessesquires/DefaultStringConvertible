d8c4a2b5d0bc5739d7f2c019c9309caf75e44937
Jesse Squires <jesse.d.squires@gmail.com>
2016-04-25 18:32:03 -0700

    the coads. omg.

--- Sources/DefaultStringConvertible.swift
+++ Sources/DefaultStringConvertible.swift
@@ -16,4 +16,43 @@
 //  Released under an MIT license: http://opensource.org/licenses/MIT
 //
 
-import Foundation
+
+/// A better default implementation of `description`. 
+/// Displays the type name followed by all members with labels.
+/// Based on [this post](http://ericasadun.com/2016/04/18/default-reflection/).
+public extension CustomStringConvertible {
+
+    /**
+     Constructs a detailed description of the receiver via its `Mirror`.
+
+     - parameter instance: The caller.
+
+     - returns: A detailed description of the receiver
+     */
+    public func defaultDescription<T>(instance: T) -> String {
+        let mirror = Mirror(reflecting: instance)
+        let chunks = mirror.children.map({
+            (label: String?, value: Any) -> String in
+            if let label = label {
+                if value is String {
+                    return "\(label): \"\(value)\""
+                }
+                return "\(label): \(value)"
+            } else {
+                return "\(value)"
+            }
+        })
+
+        if chunks.count > 0 {
+            let chunksString = chunks.joinWithSeparator(", ")
+            return "\(mirror.subjectType)(\(chunksString))"
+        }
+
+        return "\(instance)"
+    }
+
+    /// Returns the value from `defaultDescription`.
+    public var description: String {
+        return defaultDescription(self)
+    }
+}
